fbe312b1c85dd74c5b81df14b6a4a197a740018d
HelaGone <luah613@gmail.com>
2021-04-19 01:32:35 -0500

    fix page styles

--- themes/web-base-theme/page.php
+++ themes/web-base-theme/page.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
-<main id="simple_page">
+<main id="simple_page" class="main_wrapper">
   <section class="inner_wrapper">
     <?php
     if(have_posts()):
       while(have_posts()):
         the_post(); ?>
-          <h1><?php the_title(); ?></h1>
+          <h1 class="single_heading"><?php the_title(); ?></h1>
           <div class="body_container">
             <?php the_content(); ?>
           </div>
